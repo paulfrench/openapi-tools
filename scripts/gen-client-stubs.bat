956b9d4a79f4bf63fa6519015acf1afbadf13fa0
paul.french <paul.french@oneadvanced.com>
2021-06-29 11:38:50 +0100

    initial checkin

---
+++ scripts/gen-client-stubs.bat
@@ -0,0 +1,3 @@
+openapi-generator-cli generate -o generated-client-code -g java -i ../api-definitions/openapi.json --additional-properties=dateLibrary=java8,delegatePattern=true
+
+pause
---
+++ scripts/gen-server-stubs.bat
@@ -0,0 +1,3 @@
+openapi-generator-cli generate -o .. -g spring -i ../api-definitions/openapi.json --additional-properties=dateLibrary=java8,delegatePattern=true
+
+pause
